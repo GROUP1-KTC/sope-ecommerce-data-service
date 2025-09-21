47188ae26b664392fdb63fe2d2718cbc9265cdc6
Duy Khanh <162108959+this-is-duykhanh@users.noreply.github.com>
2025-09-21 10:40:01 +0700

    update entrypoint auto update

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,23 +11,44 @@ MODEL_DIR="/app/models"
 # create model directory if not exists
 mkdir -p $MODEL_DIR
 
-# List of models
-declare -A MODELS
-MODELS=( 
-  ["phobert_model"]=$PHOBERT_MODEL_PATH
-  ["semantic_model"]=$SEMANTIC_MODEL_PATH
-  ["yolo_model"]=$YOLO_MODEL_PATH
-)
+# -------------------------------
+# Download Phobert model (folder)
+# -------------------------------
+if [ -n "$PHOBERT_MODEL_PATH" ]; then
+    PHOBERT_TARGET="$MODEL_DIR/phobert_model"
+    if [ ! -d "$PHOBERT_TARGET" ]; then
+        echo "Downloading phobert_model..."
+        aws s3 cp --recursive "$PHOBERT_MODEL_PATH" "$PHOBERT_TARGET"
+    else
+        echo "phobert_model already exists, skip download."
+    fi
+fi
 
-for model in "${!MODELS[@]}"; do
-  TARGET_DIR="$MODEL_DIR/$model"
-  if [ ! -d "$TARGET_DIR" ]; then
-    echo "Downloading $model..."
-    aws s3 cp --recursive "${MODELS[$model]}" "$TARGET_DIR"
-  else
-    echo "$model already exists, skip download."
-  fi
-done
+# -------------------------------
+# Download Semantic model (folder)
+# -------------------------------
+if [ -n "$SEMANTIC_MODEL_PATH" ]; then
+    SEMANTIC_TARGET="$MODEL_DIR/reviews_emotion_model"
+    if [ ! -d "$SEMANTIC_TARGET" ]; then
+        echo "Downloading semantic_model..."
+        aws s3 cp --recursive "$SEMANTIC_MODEL_PATH" "$SEMANTIC_TARGET"
+    else
+        echo "semantic_model already exists, skip download."
+    fi
+fi
+
+# -------------------------------
+# Download YOLO model (single file)
+# -------------------------------
+if [ -n "$YOLO_MODEL_PATH" ]; then
+    YOLO_FILE="$MODEL_DIR/last.pt"
+    if [ ! -f "$YOLO_FILE" ]; then
+        echo "Downloading YOLO model..."
+        aws s3 cp "$YOLO_MODEL_PATH" "$YOLO_FILE"
+    else
+        echo "YOLO model already exists, skip download."
+    fi
+fi
 
 # Run app
 exec "$@"
